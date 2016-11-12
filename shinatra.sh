c06baed0f8a0840354f25dd982253ee09a247433
Paul.W Harvey <csirac2@gmail.com>
2016-11-12 16:05:58 +1100

    Quote $RESPONSE. Closes #3

--- shinatra.sh
+++ shinatra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
-while { echo -en $RESPONSE; } | nc -l ${1:-8080}; do
+while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
   echo "================================================"
 done
