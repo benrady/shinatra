40dfd7d941b8d6566067844343ef5d6db0b703ef
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2016-11-23 15:19:00 +0530

    Correct listener port in message

--- shinatra.sh
+++ shinatra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "starting server at: http://localhost:8080/"
+echo "starting server at: http://localhost:${1:-8080}/"
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
 while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
   echo "================================================"
