82d152e34b8d382b11a44ac1446ca47bbb2afbd5
Nir Cohen <nir36g@gmail.com>
2016-11-23 17:58:32 +0200

    Use printf to print equal signs multiple times

--- shinatra.sh
+++ shinatra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
 while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
-  echo "================================================"
+  printf '=%.0s' {1..48}; echo
 done
