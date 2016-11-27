b9073ed60216a44cb01907ff31a7737c5deca4df
Sobolev Nikita <mail@sobolevn.me>
2016-11-27 17:29:44 +0300

    Changed the shebang
    This change issues that some people have different `bash`es installed.
    Just like me: 
    
    > [17:28:16] sobolev :: MacBook-Pro-Nikita  ➜  Documents/PhpStormProjects/api ‹version_ger› » which bash
    > /usr/local/bin/bash

--- shinatra.sh
+++ shinatra.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
 while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
   echo "================================================"
